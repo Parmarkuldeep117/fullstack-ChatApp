25a000affdf997758c3779ee3173c09b6ed6017d
Parmarkuldeep117 <parmarkuldeep1280@gmail.com>
2026-01-06 15:35:26 +0530

    api

--- Frontend/src/lib/axios.js
+++ Frontend/src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios"
 
 export const api = axios.create({
-    baseURL: "https://fullstack-chatapp-wudv.onrender.com",
+    baseURL: "https://fullstack-chatapp-wudv.onrender.com/api",
     withCredentials: true
 })
