99c1a09ce1230113bcfcb6f7a95d88d31ae689e1
Parmarkuldeep117 <parmarkuldeep1280@gmail.com>
2026-01-06 14:10:51 +0530

    final

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -9,7 +9,6 @@ const messageRoute = require("./routes/message.route.js")
 const connectDb = require("./lib/db.js")
 const port = process.env.PORT
 
-const __dirname = path.resolve()
 
 app.use(express.json({ limit: "5mb" }))
 app.use(express.urlencoded({ extended: true, limit: "5mb" }))
--- Frontend/src/store/useAuthStore.js
+++ Frontend/src/store/useAuthStore.js
@@ -98,12 +98,12 @@ export const useAuthStore = create((set, get) => ({
         const { authUser } = get()
 
         if (!authUser || get().socket) return;
-        const socket = io(import.meta.env.VITE_SOCKET_URL, {
+        const socket = io(import.meta.env.MODE !== "development" ? import.meta.env.VITE_SOCKET_URL : "http://localhost:5001", {
             auth: {
                 userId: authUser._id
             }
         })
-        
+
 
         socket.on("connect", () => {
             console.log("user connected:", socket.id)
