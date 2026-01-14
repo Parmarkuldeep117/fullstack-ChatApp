f31bee221a52928e61dacf3e4a9b1175950bbf74
parmarkuldeep117 <parmarkuldeep1280@gmail.com>
2026-01-14 11:30:46 +0530

    Fix layout issues in RightSide component for mobile view

--- Frontend/src/components/RightSide.jsx
+++ Frontend/src/components/RightSide.jsx
@@ -12,14 +12,14 @@ const RightSide = () => {
 
   if (isMessageLoading) {
     return (
-      <div className={`flex-1 ${isMobile && "bg-base-200"} p-4`}>
+      <div className={`flex-1 ${isMobile && "bg-base-200 h-full"} p-4`}>
         <ChatSkeleton />
       </div>
     );
   }
 
   return (
-    <div className={`right ${isMobile && selectedUsers ? "fixed inset-0 z-100" : ""} scrollbar h-full overflow-auto border-base-200`}>
+    <div className={`right ${isMobile && selectedUsers ? "fixed inset-0 z-100 bg-base-200" : ""} scrollbar h-full overflow-auto border-base-200`}>
       {!selectedUsers ? <div className="h-full scrollbar overflow-auto"><RightSkeleton /></div> :
         <div className="flex flex-col h-full">
           <ChatHeader />
